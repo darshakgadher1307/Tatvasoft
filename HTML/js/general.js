cde7376325b10135d86c8490bba730121692b3b6
Darshak Gadher <95075558+darshakgadher1307@users.noreply.github.com>
2021-12-27 12:38:17 +0530

    Add files via upload

--- HTML/js/general.js
+++ HTML/js/general.js
@@ -9,10 +9,42 @@ $(document).ready(function(){
             $("header").removeClass("fixed");  	
         }
     })
-  })
 
-$(document).ready(function(){
   $(".nav-tabs a").click(function(){
     $(this).tab('show');
   });
+
+  $(window).resize(function(){
+    if($(window).width()<1200){
+      $("#upcoming-service-menu-table").addClass("flex-column");
+      $("#upcoming-service-menu").removeClass("flex-column");
+      $("#upcoming-service-menu").addClass("flex-row justify-content-center");
+    }
+    else{
+      $("#upcoming-service-menu-table").removeClass("flex-column");
+      $("#upcoming-service-menu").addClass("flex-column");
+      $("#upcoming-service-menu").removeClass("flex-row justify-content-center");
+    } 
+    if($(window).width()<992){
+      $("#nav-line").removeClass("line");
+      $("#l1").removeClass("line");
+    }
+    else{
+      $("#nav-line").addClass("line");
+      $("#l1").addClass("line");
+    }
+    if($(window).width()<718){
+      $("tr").addClass("d-flex");
+      $("tr").addClass("flex-column");
+      $("td").addClass("text-center");
+      $("#table-row-number").addClass("flex-column")
+    }
+    else{
+      $("tr").removeClass("d-flex");
+      $("tr").removeClass("flex-column");
+      $("td").removeClass("text-center");
+      $("#table-row-number").removeClass("flex-column")
+    }
+  });
 });
+
